1b7a20b042f8508d7a584266ec74fc68fe6c4663
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-03-03 11:52:43 -0600

    Creación de la clase main, atributos, getters y setters

--- src/main/java/Circulo.java
+++ src/main/java/Circulo.java
@@ -1,3 +1,16 @@
 public class Circulo {
+    public double radio;
 
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double area(){
+        int area;
+        area=
+    }
 }
